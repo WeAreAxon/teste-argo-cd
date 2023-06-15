27c0eb98b2864a6d17e038e64730e22f2c4be280
DevOps Axon Technology <axon.technology@outlook.com>
2023-06-15 16:05:47 -0300

    willian

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "net/http"
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("<h1>Hello Damisao</h1>"))
+		w.Write([]byte("<h1>Willainsao/h1>"))
 	})
 	http.ListenAndServe(":8080", nil)
 
