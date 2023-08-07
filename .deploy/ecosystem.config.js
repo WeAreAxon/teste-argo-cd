05a4cd4cb57eeb4503d04e217ca22ef28ee706db
Axon Technology <120113658+axon-technology@users.noreply.github.com>
2023-08-07 14:22:43 -0300

    Update ecosystem.config.js
    Signed-off-by: Axon Technology <120113658+axon-technology@users.noreply.github.com>

--- .deploy/ecosystem.config.js
+++ .deploy/ecosystem.config.js
@@ -1,29 +1,63 @@
 module.exports = {
-    apps: 
-    [
-        {
-            name: "connect-api:5800",
-            script: "../server/dist/api/server-refac.js",
-            exec_mode: "fork",
-            instances: 1,
-            env: {
-                NODE_ENV: "connectelopment",
-                MYSQL_HOST: "connect_mysql57",
-                MYSQL_PORT: "3306",
-                MYSQL_USER: "connect",
-                MYSQL_PASSWORD: "bpWAE7eZlKPBJMP",
-                MYSQL_DATABASE: process.env.MYSQL_DATABASE,
-                MONGODB_URI: "mongodb://axon:3lirIlN43qdhjLRU@134.122.115.13:27017/?retryWrites=true&w=majority",
-                MAIL_HOST: "smtp-mail.outlook.com",
-                NODEMAILER_USER: "comunica@martiaco.com.br",
-                NODEMAILER_PASS: "Cok42166",
-                JWT_SECRET: "683OEWtQBb5758c1-c2efc4fd678IUGHt5bfa630d74-b8a004e342aa507b9f5PL",
-                BACK_END_ROUTE: "http://localhost:5800",
-                QUEUEHANDLER_ROUTE: "http://localhost:3050",
-                PORT_API_CAPTURE: 5800
-            },
-            "watch": true,
-            "min_uptime": 5000
-        },
-    ],
+  apps: [
+    {
+      name: "connect-api:5800",
+      script: "../server/dist/api/server-refac.js",
+      exec_mode: "fork",
+      instances: 1,
+      env: {
+        NODE_ENV: "connectelopment",
+        MYSQL_HOST: process.env.MYSQL_HOST,
+        MYSQL_PORT: "3306",
+        MYSQL_USER: process.env.MYSQL_USER,
+        MYSQL_PASSWORD: process.env.MYSQL_PASSWORD,
+        MYSQL_DATABASE: process.env.MYSQL_DATABASE,
+        MONGODB_URI: process.env.MONGODB_URI,
+        MAIL_HOST: process.env.MAIL_HOST,
+        NODEMAILER_USER: process.env.NODEMAILER_USER,
+        NODEMAILER_PASS: process.env.NODEMAILER_PASS,
+        JWT_SECRET: process.env.JWT_SECRET,
+        BACK_END_ROUTE: "http://localhost:5800",
+        QUEUEHANDLER_ROUTE: "http://localhost:3050",
+        PORT_API_CAPTURE: 5800
+      },
+      watch: true,
+      min_uptime: 5000
+    },
+    {
+      name: "connect-translate-signals",
+      script: "../server/dist/translate-signals/index.js",
+      exec_mode: "fork",
+      instances: 1,
+      min_uptime: 5000,
+      watch: true,
+      env: {
+        NODE_ENV: "connectelopment",
+        MYSQL_HOST: process.env.MYSQL_HOST,
+        MYSQL_PORT: "3306",
+        MYSQL_USER: process.env.MYSQL_USER,
+        MYSQL_PASSWORD: process.env.MYSQL_PASSWORD,
+        MYSQL_DATABASE: process.env.MYSQL_DATABASE,
+        MONGODB_URI: process.env.MONGODB_URI
+      }
+    },
+    {
+      name: "connect-webhook-mqtt:5100",
+      script: "../server/dist/webhook-mqtt/server.js",
+      exec_mode: "fork",
+      instances: 1,
+      min_uptime: 5000,
+      watch: true,
+      env: {
+        NODE_ENV: "connectelopment",
+        MYSQL_HOST: process.env.MYSQL_HOST,
+        MYSQL_PORT: "3306",
+        MYSQL_USER: process.env.MYSQL_USER,
+        MYSQL_PASSWORD: process.env.MYSQL_PASSWORD,
+        MYSQL_DATABASE: process.env.MYSQL_DATABASE,
+        MONGODB_URI: process.env.MONGODB_URI,
+        PORT: 5100
+      }
+    }
+  ]
 };
